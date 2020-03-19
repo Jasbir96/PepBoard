6ed58126e02b8e0fd69c62a2281eb09b9bed840f
Jasbir <mr.jasbirsingh96@gmail.com>
2020-03-19 06:28:32 +0530

    added favicon as well as new links

--- public/js/tool.js
+++ public/js/tool.js
@@ -1,5 +1,5 @@
 // connect to socket server
-const socket = io.connect("http://localhost:3000");
+const socket = io.connect("https://pepboard.herokuapp.com/");
 // *********************************Basic Setup
 const board = document.querySelector(".board");
 board.height = window.innerHeight;
