50bdff20900731a84ebbecc3cacf6d3666772509
Jasbir <mr.jasbirsingh96@gmail.com>
2020-03-19 06:23:26 +0530

    changed port for deployment

--- express.js
+++ express.js
@@ -12,23 +12,23 @@ app.use(express.static("public"));
 // server
 io.on("connection", function(socket) {
   socket.on("size", function(size) {
-    socket.broadcast.emit("onsize",size);
+    socket.broadcast.emit("onsize", size);
   });
   socket.on("color", function(color) {
-    socket.broadcast.emit("oncolor",color);
+    socket.broadcast.emit("oncolor", color);
   });
-  
+
   socket.on("toolchange", function(tool) {
-    socket.broadcast.emit("ontoolchange",tool);
+    socket.broadcast.emit("ontoolchange", tool);
   });
   socket.on("hamburger", function() {
     socket.broadcast.emit("onhamburger");
   });
   socket.on("mousedown", function(point) {
-    socket.broadcast.emit("onmousedown",point);
+    socket.broadcast.emit("onmousedown", point);
   });
   socket.on("mousemove", function(point) {
-    socket.broadcast.emit("onmousemove",point);
+    socket.broadcast.emit("onmousemove", point);
   });
   socket.on("undo", function() {
     socket.broadcast.emit("onundo");
@@ -38,6 +38,7 @@ io.on("connection", function(socket) {
   });
 });
 // nodejs server
-server.listen(3000, function(req, res) {
+const port = process.env.PORT || 3000;
+server.listen(port, function(req, res) {
   console.log("Server has started at port 3000");
 });
