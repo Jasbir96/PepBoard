5d05924cdaa027bed807158f735ceceab78bbd30
Jasbir Singh <mr.jasbirsingh96@gmail.com>
2024-02-21 19:54:54 +0530

    Update remote backend server

--- public/js/tool.js
+++ public/js/tool.js
@@ -1,5 +1,5 @@
 // connect to socket server
-const socket = io.connect("https://pepboard.herokuapp.com/");
+const socket = io.connect("https://pepboard-3fhn.onrender.com");
 // *********************************Basic Setup
 const board = document.querySelector(".board");
 board.height = window.innerHeight;
